69dfbea2e6832d4b4b19dbe41da000d38aa292a6
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2024-04-02 17:34:14 +0530

    Updates

--- static/js/812.d1566b5c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkceew=self.webpackChunkceew||[]).push([[812],{812:(e,a,c)=>{c.r(a),c.d(a,{default:()=>d});c(43);var s=c(579);const d=()=>(0,s.jsx)("div",{children:"DataDownloadPage"})}}]);
-//# sourceMappingURL=812.d1566b5c.chunk.js.map
